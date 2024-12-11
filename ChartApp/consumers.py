57ab5b1cf62936bb88c751a03f87ca8920e244d4
ramankumar-BackendDevelopement <ramankumard444@gmail.com>
2024-12-11 11:48:07 +0530

    success

---
+++ ChartApp/consumers.py
@@ -0,0 +1,58 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Room, Message
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"room_{self.room_name}"
+        
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        sender = text_data_json['sender']
+        room_name = text_data_json['room_name']
+
+        # Save message to database
+        await self.create_message(room_name, sender, message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender': sender
+            }
+        )
+
+    @database_sync_to_async
+    def create_message(self, room_name, sender, message):
+        room = Room.objects.get(room_name=room_name)
+        Message.objects.create(room=room, sender=sender, message=message)
+
+    async def chat_message(self, event):
+        message = event['message']
+        sender = event['sender']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'sender': sender
+        }))
